dd94434c19f00af7e6de479c53cd91ec05a00c8a
forgetSalary <chrisfly424@gmail.com>
2020-09-11 04:07:36 +0300

    Added report

--- task5.c
+++ task5.c
@@ -1,39 +1,41 @@
 #include "task5.h"
 
 void searchByFullName(struct student* base, int baseSize, char famil[20],char name[20]){
-    int found=0;
+    int count=0;
 
+    printf("Poisk po imeni '%s %s'\n",famil,name);
     for (int i=0; i<baseSize; i++){
         if (strcmp(base[i].famil,famil)==0){
             if (strcmp(base[i].name,name)==0){
-                printf("Student:\nFamiliya:%s\nImya:%s\nFacultet:%s\nNomer zachetki:%d\n\n",
-                       base[i].famil,base[i].name,base[i].facult,base[i].Nomzach);
-                found=1;
+                printf("Student%d:\nFamiliya: %s\nImya: %s\nFacultet: %s\nNomer zachetki: %d\n\n",
+                       count+1,base[i].famil,base[i].name,base[i].facult,base[i].Nomzach);
+                count+=1;
             }
 
         }
 
     }
 
-    if (found==0){
+    if (count==0){
         printf("\nSudenti ne naydeni\n");
     }
 }
 
 void searchByFacult(struct student* base, int baseSize, char facult[20]){
-    int found=0;
+    int count=0;
 
+    printf("Poisk po fakultetu '%s'\n",facult);
     for (int i=0; i<baseSize; i++){
         if (strcmp(base[i].facult,facult)==0){
-            printf("Student:\nFamiliya:%s\nImya:%s\nFacultet:%s\nNomer zachetki:%d\n\n",
-                   base[i].famil,base[i].name,base[i].facult,base[i].Nomzach);
-            found=1;
+            printf("Student%d:\nFamiliya: %s\nImya: %s\nFacultet: %s\nNomer zachetki: %d\n\n",
+                   count+1,base[i].famil,base[i].name,base[i].facult,base[i].Nomzach);
+            count+=1;
 
         }
 
     }
 
-    if (found==0){
+    if (count==0){
         printf("Sudenti ne naydeni");
     }
 }
@@ -48,6 +50,8 @@ void example5(){
     searchByFullName(base,5,"Khanbekova","Elena");
 
     searchByFacult(base,5,"MT");
+
+    searchByFacult(base,5,"VT");
 
     searchByFacult(base,5,"facult");
 }
